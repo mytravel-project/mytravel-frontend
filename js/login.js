84237681c7dae950eda2053be957b153431d5683
song_eun <dmswo546@gmail.com>
2025-03-17 23:04:26 +0900

    chore: 프론트엔드 초기 세팅

---
+++ js/login.js
@@ -0,0 +1,73 @@
+const openLoginButton = document.getElementById("openLoginButton");
+const logoutButton = document.getElementById("logoutButton");
+const submitLoginButton = document.getElementById("submitLoginButton");
+const loginMessage = document.getElementById("loginMessage");
+const openSignupButton = document.getElementById("openSignupButton");
+
+submitLoginButton.addEventListener("click", async () => {
+  const email = document.getElementById("loginEmail").value;
+  const password = document.getElementById("loginPassword").value;
+
+  if (!email || !password) {
+    loginMessage.textContent = "이메일 또는 비밀번호를 입력하세요.";
+    return;
+  }
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  if (!emailRegex.test(email)) {
+    loginMessage.textContent =
+      "유효하지 않은 이메일 형식입니다. 다시 입력해주세요.";
+    return;
+  }
+
+  let response = await axios.post("http://localhost:8080/tokenLogin", {
+    email,
+    password,
+  });
+
+  const token = response.data.Authorization;
+  const nickname = response.data.nickname;
+
+  if (token && nickname) {
+    alert("로그인되었습니다.");
+    sessionStorage.setItem("Authorization", token);
+    sessionStorage.setItem("nickname", nickname);
+    axios.defaults.headers.common["Authorization"] = token;
+    openLoginButton.classList.add("hidden");
+    openSignupButton.classList.add("hidden");
+    logoutButton.classList.remove("hidden");
+
+    const modal = bootstrap.Modal.getInstance(
+      document.getElementById("loginModal")
+    );
+    loginModal.setAttribute("aria-hidden", "true");
+    modal.hide();
+    window.location.reload();
+  } else {
+    loginMessage.textContent = response.data.msg;
+  }
+});
+
+logoutButton.addEventListener("click", async () => {
+  await axios.post("http://localhost:8080/logout");
+  sessionStorage.removeItem("nickname");
+  sessionStorage.removeItem("Authorization");
+  axios.defaults.headers.common["Authorization"] = "";
+  window.location.reload();
+});
+
+const setLoginState = () => {
+  let Authorization = sessionStorage.getItem("Authorization");
+  const nickname = sessionStorage.getItem("nickname");
+
+  if (Authorization && nickname) {
+    axios.defaults.headers.common["Authorization"] = Authorization;
+    document.getElementById("loginSpan").innerHTML = `${nickname}`;
+    openLoginButton.classList.add("hidden");
+    openSignupButton.classList.add("hidden");
+    logoutButton.classList.remove("hidden");
+  }
+};
+
+setLoginState();
---
+++ js/register.js
@@ -0,0 +1,286 @@
+document.getElementById("nickname").addEventListener("input", isValidNickname);
+document.getElementById("nickname").addEventListener("blur", async () => {
+  if (!isValidNickname()) return;
+  const nickname = document.getElementById("nickname").value.trim();
+  await checkNicknameDuplicate(nickname);
+});
+
+document.getElementById("signupEmail").addEventListener("input", isValidEmail);
+document.getElementById("signupEmail").addEventListener("blur", async () => {
+  if (!isValidEmail()) return;
+  const email = document.getElementById("signupEmail").value.trim();
+  await checkEmailDuplicate(email);
+});
+
+document
+  .getElementById("signupPassword")
+  .addEventListener("input", isValidPassword);
+document
+  .getElementById("signupPassword")
+  .addEventListener("blur", isValidPassword);
+
+document
+  .getElementById("passwordCheck")
+  .addEventListener("input", checkPassword);
+document
+  .getElementById("passwordCheck")
+  .addEventListener("blur", checkPassword);
+
+// 가입 버튼
+document
+  .getElementById("signupBtn")
+  .addEventListener("click", async (event) => {
+    event.preventDefault(); // 제출 시 새로고침 방지
+
+    const nickname = document.getElementById("nickname").value.trim();
+    const email = document.getElementById("signupEmail").value.trim();
+    const password = document.getElementById("signupPassword").value.trim();
+    const passwordCheck = document.getElementById("passwordCheck").value.trim();
+
+    const birthYearElement = document.getElementById("birth-year");
+    const birth_year = birthYearElement ? birthYearElement.value : null;
+
+    const genderElement = document.querySelector(
+      'input[name="gender"]:checked'
+    );
+    const gender = genderElement ? genderElement.value : null;
+
+    const residence_state = document.getElementById("residenceState").value;
+    const residence_city = document.getElementById("residenceCity").value;
+
+    // 필수 항목 입력 확인
+    if (
+      !nickname ||
+      !email ||
+      !password ||
+      !passwordCheck ||
+      !gender ||
+      !birth_year ||
+      !residence_state ||
+      !residence_city
+    ) {
+      alert("모든 필드를 입력해야 합니다.");
+      return;
+    }
+
+    // 유효성 검사 실행
+    const isNicknameValid = isValidNickname();
+    const isNicknameExists = await checkNicknameDuplicate(nickname);
+
+    const isEmailValid = isValidEmail();
+    const isEmailExists = await checkEmailDuplicate(email);
+
+    const isPasswordValid = isValidPassword();
+    const isPasswordMatch = checkPassword();
+
+    // 모든 유효성 검사를 통과해야 회원가입 요청 보내기
+    if (
+      !isNicknameValid ||
+      isNicknameExists ||
+      !isEmailValid ||
+      isEmailExists ||
+      !isPasswordValid ||
+      !isPasswordMatch
+    ) {
+      return; // 요청 차단
+    }
+
+    const data = {
+      nickname,
+      password,
+      email,
+      gender,
+      birth_year,
+      residence_state,
+      residence_city,
+    };
+
+    try {
+      const response = await axios.post("http://localhost:8080/register", data);
+
+      document.getElementById("effectMsg").innerHTML = response.data;
+
+      if (response.data === "환영합니다") {
+        const modalElement = document.getElementById("signupModal");
+        let modal = bootstrap.Modal.getInstance(modalElement);
+        if (!modal) {
+          modal = new bootstrap.Modal(modalElement);
+        }
+        modal.hide();
+      }
+    } catch (error) {
+      console.error("회원가입 요청 실패:", error);
+      alert("회원가입 중 오류가 발생했습니다. 다시 시도해주세요.");
+    }
+  });
+
+// 가입 취소 버튼
+document.getElementById("signupCancelBtn").addEventListener("click", () => {
+  const modalElement = document.getElementById("signupModal");
+  let modal = bootstrap.Modal.getInstance(modalElement);
+  if (!modal) {
+    modal = new bootstrap.Modal(modalElement);
+  }
+  modal.hide();
+});
+
+document.getElementById("nickname").addEventListener("blur", async () => {
+  const nickname = document.getElementById("nickname").value.trim();
+  await checkNicknameDuplicate(nickname);
+});
+
+// 닉네임 중복 검사
+let isCheckingNickname = false;
+
+async function checkNicknameDuplicate(nickname) {
+  const nicknameError = document.getElementById("nicknameError");
+  const nicknameInput = document.getElementById("nickname");
+
+  if (!nickname) return false;
+
+  if (isCheckingNickname) return; // 중복 실행 방지
+  isCheckingNickname = true;
+
+  try {
+    const response = await axios.post(
+      "http://localhost:8080/isNicknameExists",
+      { nickname }
+    );
+
+    if (response.data.exists) {
+      nicknameError.textContent = "이미 사용 중인 닉네임입니다.";
+      nicknameInput.style.border = "1px solid red";
+      return false;
+    } else {
+      nicknameError.textContent = "";
+      nicknameInput.style.border = "1px solid #ccc";
+      return true;
+    }
+  } catch (error) {
+    console.error("오류 발생:", error);
+    return false;
+  }
+}
+
+// 닉네임 유효성 검사
+function isValidNickname() {
+  const nicknameInput = document.getElementById("nickname");
+  const nicknameError = document.getElementById("nicknameError");
+  const nickname = nicknameInput.value.trim();
+
+  const nicknamePattern = /^[a-zA-Z0-9가-힣]{2,15}$/; //2자리 이상 15자리 이하, 특수문자 X
+
+  if (!nickname) {
+    nicknameError.textContent = "닉네임을 입력해주세요.";
+    nicknameInput.style.border = "1px solid red";
+    return false;
+  } else if (!nicknamePattern.test(nickname)) {
+    nicknameError.textContent =
+      "닉네임은 2자리 이상 15자리 이하이며, 특수문자를 포함할 수 없습니다.";
+    nicknameInput.style.border = "1px solid red";
+    return false;
+  } else {
+    nicknameError.textContent = "";
+    nicknameInput.style.border = "1px solid #ccc";
+    return true;
+  }
+}
+
+//이메일 유효성 검사
+function isValidEmail() {
+  const emailInput = document.getElementById("signupEmail");
+  const emailError = document.getElementById("emailError");
+  const email = emailInput.value.trim();
+
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+  if (!email) {
+    emailError.textContent = "이메일을 입력해주세요.";
+    emailInput.style.border = "1px solid red";
+    return false;
+  } else if (!emailPattern.test(email)) {
+    emailError.textContent =
+      "유효하지 않은 이메일 형식입니다. 다시 입력해주세요.";
+    emailInput.style.border = "1px solid red";
+    return false;
+  } else {
+    emailError.textContent = "";
+    emailInput.style.border = "1px solid #ccc";
+    return true;
+  }
+}
+
+// 이메일 중복 검사
+let isCheckingEmail = false;
+
+async function checkEmailDuplicate(email) {
+  const emailError = document.getElementById("emailError");
+  const emailInput = document.getElementById("signupEmail");
+
+  if (!email) return false;
+
+  if (isCheckingEmail) return; // 중복 실행 방지
+  isCheckingEmail = true;
+
+  try {
+    const response = await axios.post("http://localhost:8080/isEmailExists", {
+      email,
+    });
+
+    if (response.data.exists) {
+      emailError.textContent =
+        "이미 가입된 이메일입니다. 다른 이메일을 입력해주세요.";
+      emailInput.style.border = "1px solid red";
+      return false;
+    } else {
+      emailError.textContent = "";
+      emailInput.style.border = "1px solid #ccc";
+      return true;
+    }
+  } catch (error) {
+    console.error("이메일을 확인하는 중 오류가 발생하였습니다: ", error);
+    return false;
+  }
+}
+
+// 비밀번호 유효성 검사
+function isValidPassword() {
+  const passwordInput = document.getElementById("signupPassword");
+  const passwordError = document.getElementById("passwordError");
+  const password = passwordInput.value;
+
+  const passwordPattern =
+    /^(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/;
+
+  if (!password) {
+    passwordError.textContent = "비밀번호를 입력해주세요.";
+    passwordInput.style.border = "1px solid red";
+    return false;
+  } else if (!passwordPattern.test(password)) {
+    passwordError.textContent =
+      "비밀번호는 특수문자, 숫자 포함 8자리 이상이어야 합니다.";
+    passwordInput.style.border = "1px solid red";
+    return false;
+  } else {
+    passwordError.textContent = "";
+    passwordInput.style.border = "1px solid #ccc";
+    return true;
+  }
+}
+
+// 비밀번호 일치 검사
+function checkPassword() {
+  const passwordInput = document.getElementById("signupPassword");
+  const passwordCheckInput = document.getElementById("passwordCheck");
+  const passwordCheckError = document.getElementById("passwordCheckError");
+
+  if (passwordInput.value !== passwordCheckInput.value) {
+    passwordCheckError.textContent = "비밀번호가 일치하지 않습니다.";
+    passwordCheckInput.style.border = "1px solid red";
+    return false;
+  } else {
+    passwordCheckError.textContent = "";
+    passwordCheckInput.style.border = "1px solid #ccc";
+    return true;
+  }
+}
